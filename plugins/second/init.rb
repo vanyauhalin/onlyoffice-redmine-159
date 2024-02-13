fa7c8171f10df6603492f25695a3393bdc9d2a80
vanyauhalin <vanyauhalin@gmail.com>
2024-02-13 12:30:58 +0400

    add init

---
+++ plugins/second/init.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "redmine"
+
+Redmine::Plugin.register(:second) do
+  name        "second"
+  author      "vanyauhalin"
+  description "vanyauhalin"
+  version     "0.0.1"
+  url         "https://google.com/"
+  author_url  "https://google.com/"
+end
---
+++ plugins/third/init.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "redmine"
+
+Redmine::Plugin.register(:third) do
+  name        "third"
+  author      "vanyauhalin"
+  description "vanyauhalin"
+  version     "0.0.1"
+  url         "https://google.com/"
+  author_url  "https://google.com/"
+end
